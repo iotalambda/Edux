4977d005afc453c69260306e9f48920ff73064fb
Joona Luoma <joona.luoma@hotmail.com>
2025-02-25 14:05:56 +0200

    Format ts

--- Web/Scripts/edux-scripts/src/eduxjs.ts
+++ Web/Scripts/edux-scripts/src/eduxjs.ts
@@ -3,23 +3,27 @@ import { IEduxJsAdapter } from "./typings";
 let eventHandlerJs: string = "{}";
 
 function handleEduxEvent(eduxEventKey: string) {
-    eval(eventHandlerJs);
+  eval(eventHandlerJs);
 }
 
 function setEventHandlerJs(value: string) {
-    eventHandlerJs = value;
+  eventHandlerJs = value;
 }
 
 function resetEduxRoot() {
-    document.getElementById('edux-root')?.remove();
-    const container = document.getElementById('edux-root-container');
-    if (!container) {
-        console.error('EDUX: Element edux-root-container not found!');
-        return;
-    }
-    const root = document.createElement('div');
-    root.id = "edux-root";
-    container.appendChild(root);
+  document.getElementById("edux-root")?.remove();
+  const container = document.getElementById("edux-root-container");
+  if (!container) {
+    console.error("EDUX: Element edux-root-container not found!");
+    return;
+  }
+  const root = document.createElement("div");
+  root.id = "edux-root";
+  container.appendChild(root);
 }
 
-export default { handleEduxEvent, setEventHandlerJs, resetEduxRoot } as IEduxJsAdapter
+export default {
+  handleEduxEvent,
+  setEventHandlerJs,
+  resetEduxRoot,
+} as IEduxJsAdapter;
--- Web/Scripts/edux-scripts/src/jsutils.ts
+++ Web/Scripts/edux-scripts/src/jsutils.ts
@@ -2,15 +2,15 @@ import { IJsUtilsAdapter } from "./typings";
 
 async function blur() {
   if (document.activeElement && document.activeElement !== document.body) {
-      (document.activeElement as any).blur();
+    (document.activeElement as any).blur();
   }
 }
 
 async function scrollToBottom(elementId: string) {
   const el = document.getElementById(elementId);
   if (el) {
-    el.scrollTo({ behavior: "smooth", top: el.scrollHeight })
+    el.scrollTo({ behavior: "smooth", top: el.scrollHeight });
   }
 }
 
-export default { blur, scrollToBottom } as IJsUtilsAdapter
+export default { blur, scrollToBottom } as IJsUtilsAdapter;
